51a03bb7d7b91c628c21012bb761bfd0f78fb9cf
ZjHOU <z@zjh.im>
2020-08-02 01:28:47 +0800

    chore: use res from bblego

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 export * from './components'
-export * from './icons'
+
+import RulerIcon from './icons/ruler.svg';
+import TagIcon from './icons/tag.svg';
+import EditIcon from './icons/edit.svg';
+import PostIcon from './icons/post.svg';
+import AboutIcon from './icons/about.svg';
+
+export {
+  RulerIcon,
+  TagIcon,
+  EditIcon,
+  PostIcon,
+  AboutIcon
+}
 

66900577723d1bc8a822ec500260ae6f710035a9
bbbottle <z@zjh.im>
2020-10-31 12:28:21 +0800

    enable fullscreen menu-item

--- src/components/menu/menu_item.js
+++ src/components/menu/menu_item.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import cn from 'classnames';
+
 
 import { IconText } from "../icon_text";
 import { COLORS } from "../..";
@@ -7,6 +9,7 @@ import { COLORS } from "../..";
 export const MenuItem = (props) => {
   const {
     title,
+    isFullScreen,
     showChildren,
     children,
   } = props;
@@ -18,7 +21,9 @@ export const MenuItem = (props) => {
         color={COLORS.$green1}
         className="panel-title"
       />
-      <div className="menu-item-content">
+      <div className={cn("menu-item-content", {
+        fullScreen: isFullScreen,
+      })}>
         {showChildren && children}
       </div>
     </div>
